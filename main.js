a2d75c494d044b0fe8d5fa1754ad2b2f72335d79
armandosegura <segura_arm@yahoo.com>
2018-02-07 15:40:06 -0600

    added type selection, added search bar functionality

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 "use strict"
 
+
+
+function myFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myUL");
+    li = ul.getElementsByTagName("li");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+
+        }
+    }
+}
+
 function renderCoffee(coffee) {
     var html = '<tr class="coffee">';
     html += '<td>' + coffee.id + '</td>';
